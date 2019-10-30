f5523d7d276bdcaaa0ab304f31585d89e6db5d2a
Callum Tolley <callum.tolley@ocado.com>
2019-10-30 11:46:17 +0000

    Add global git hooks

--- bash/git_config.sh
+++ bash/git_config.sh
@@ -38,6 +38,7 @@ function config_git {
 	git config --global push.default simple                         # Adopt new pushing format
 
 	git config --global core.excludesFile '~/.dotfiles/git/global_gitignore'
+	git config --global core.hooksPath '~/.dotfiles/git/hooks'
 }
 $_INTERACTIVE_SHELL && $_LOGIN_SHELL && config_git 2>&1 >/dev/null
 
