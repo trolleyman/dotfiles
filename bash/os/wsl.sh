0e00af8c4871f39c92c64a4ecabd95f7a7aec173
Callum Tolley <callum.tolley@ocado.com>
2020-05-22 11:06:06 +0100

    Add 'cdw' command in WSL mode

--- bash/os/wsl.sh
+++ bash/os/wsl.sh
@@ -22,3 +22,26 @@ cmd() (
     cmd.exe /C "${args[@]}"
 )
 
+# TODO: Override 'cd' to handle Windows paths
+
+cdw() {
+    if [[ $# == 0 ]]; then
+        cd ~
+    elif [[ $# > 1 ]]; then
+        echo "cdw: too many arguments" 1>&2
+        return 1
+    else
+        if [[ "$1" == '~' ]]; then
+            # TODO: Handle '~/Downloads', etc. properly
+            cd "/mnt/c/Users/$(whoami)"
+            return
+        fi
+        path="$(wslpath "$1" 2>&1)"
+        if [[ $? != 0 ]]; then
+            echo "wslpath: invalid Windows path: $1"
+            return 1
+        fi
+        cd "$path"
+    fi
+}
+
