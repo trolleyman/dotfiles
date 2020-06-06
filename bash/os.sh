6d5ef07249764c103821035de1b5d7edac05fef5
Callum Tolley <cgtrolley@gmail.com>
2020-06-06 19:57:40 +0100

    Add wsl1/2 distinction, and change git replacement to be only for WSL2

--- bash/os.sh
+++ bash/os.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
 # Source os/ scripts
-if grep -q -i Microsoft /proc/version; then
+if grep -qi microsoft /proc/version; then
     # WSL
-    export PATH=~/.dotfiles/bin/os/wsl:$PATH
+    if [[ -d ~/.dotfiles/bin/os/wsl ]]; then
+        export PATH=~/.dotfiles/bin/os/wsl:$PATH
+    fi
     if [[ -r ~/.dotfiles/bash/os/wsl.sh ]]; then
         . ~/.dotfiles/bash/os/wsl.sh
+    fi
+
+    if [[ ! -d /run/wsl ]]; then
+        # WSL1
+        if [[ -d ~/.dotfiles/bin/os/wsl1 ]]; then
+            export PATH=~/.dotfiles/bin/os/wsl1:$PATH
+        fi
+        if [[ -r ~/.dotfiles/bash/os/wsl1.sh ]]; then
+            . ~/.dotfiles/bash/os/wsl1.sh
+        fi
+    else
+        # WSL2
+        if [[ -d ~/.dotfiles/bin/os/wsl2 ]]; then
+            export PATH=~/.dotfiles/bin/os/wsl2:$PATH
+        fi
+        if [[ -r ~/.dotfiles/bash/os/wsl2.sh ]]; then
+            . ~/.dotfiles/bash/os/wsl2.sh
+        fi
     fi
 fi
 
