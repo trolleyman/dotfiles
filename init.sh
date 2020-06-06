a112baf554b1de37e65d626eed7b075c7a6439c7
Callum Tolley <cgtrolley@gmail.com>
2020-06-06 15:54:28 +0100

    Improve scripts

--- init.sh
+++ init.sh
@@ -4,6 +4,11 @@ set -ex
 
 # TODO: Handle installing Git, bash, etc.
 git clone https://github.com/trolleyman/dotfiles ~/.dotfiles
-cd ~/.dotfiles
 
-./bin/
+pushd ~/.dotfiles
+git submodule init
+git submodule sync
+git submodule update
+popd
+
+~/.dotfiles/bin/dotfiles-setup -f
