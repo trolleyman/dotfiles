7c18e5cd56abe336767c2450e9a12d039a13fdc3
Callum Tolley <cgtrolley@gmail.com>
2020-06-06 20:16:28 +0100

    Add git config to PowerShell profile

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -23,3 +23,19 @@ git config --global alias.graph 'log --oneline --decorate --color --graph --all'
 git config --global alias.graphq 'log --oneline --decorate --color --graph'
 
 git config --global alias.sm submodule
+
+git config --global alias.pullall '!f(){ git pull "$@" && git submodule update --init --recursive; }; f'
+#git config --global alias.prune "!bash -c `"f(){ git fetch -p && (git branch -r | awk '{print \\\`$1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print \\\`$1}' | xargs -r git branch -d); }; f`""
+
+# Git config
+$NumCpus = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
+git config --global color.ui auto                               # Enable colors
+git config --global submodule.recurse true                      # Recurse through submodules when pulling by default
+git config --global submodule.fetchJobs $NumCpus                # Parallelize submodule fetching
+git config --global fetch.parallel $NumCpus                     # Parallelize regular fetching
+git config --global core.preloadIndex true                      # Preload index -- helps on NFS
+git config --global push.default simple                         # Adopt new pushing format
+git config --global core.safecrlf false                         # Silence CRLF warnings
+
+git config --global core.excludesFile "$HOME\.dotfiles\git\global_gitignore"
+git config --global core.hooksPath "$HOME\.dotfiles\git\hooks"
